af37871e6d81c72deb9c414874324a3265539e19
Connor McCarthy and Kenneth Du <connor@>
2018-09-17 11:31:26 -0700

    add initial files

---
+++ WorldData/Models/WorldData.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using WorldData;
+using System;
+
+namespace WorldData.Models
+{
+  public class World
+  {
+    private int _id = 0;
+    private string _country;
+    private string _city;
+
+    public World (int id, string country, string city)
+    {
+      _id = id;
+      _country = country;
+      _city = city;
+    }
+    public void SetCountry(string country)
+    {
+      _country= country;
+    }
+    public void SetCity (string city)
+    {
+      _city= city;
+    }
+    public string GetCountry()
+    {
+      return _country;
+    }
+    public string GetCity()
+    {
+      return _city;
+    }
+    public static List<World> GetAll()
+    {
+      List<World> allItems = new List<World> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM world;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int worldId = rdr.GetInt32(0);
+        string worldCountry = rdr.GetString(1);
+        string worldCity = rdr.GetString(1);
+        World newWorld = new World(worldId, worldCountry, worldCity);
+        allItems.Add(newWorld);
+      }
+      conn.Close();
+      if(conn != null)
+      {
+        conn.Dispose();
+      }
+      return allItems;
+    }
+  }
+}
